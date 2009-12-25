70c333f38c40cc3e09c9434b766b9019217175ad
Ryan Morton <rjmorton@gmail.com>
2009-12-25 18:09:11 -0500

    * Modification of LinkParser regular expression to be less greedy. * Addition of build.xml

--- src/main/scala/Scrawler.scala
+++ src/main/scala/Scrawler.scala
@@ -7,11 +7,11 @@ import action.LinkParser
  * A main function that boostraps crawling efforts
  */
 object Scrawler {
-  def main(args : Array[String]) : Unit = {
-    //val sourceUrl = "http://192.168.1.105/~rynmrtn/"
-    val sourceUrl = "http://www.google.com/"
-    val source = Source.fromURL(new URL(sourceUrl))
-    val lst = LinkParser.parse(sourceUrl, source)
-    lst.foreach { link => println(link.toString) }
-  }
+    def main(args : Array[String]) : Unit = {
+        //val sourceUrl = "http://192.168.1.105/~rynmrtn/"
+        val sourceUrl = "http://www.google.com/"
+        val source = Source.fromURL(new URL(sourceUrl))
+        val lst = LinkParser.parse(sourceUrl, source)
+        lst.foreach { link => println(link.toString) }
+    }
 }
--- src/main/scala/action/LinkParser.scala
+++ src/main/scala/action/LinkParser.scala
@@ -7,18 +7,29 @@ import scala.xml._
 import domain.Link
 
 /**
+ *  Singleton parser designed to take a Source and pull all
+ *  links from the text of the url.
  *
+ *  This is rather simplistic and finds any html anchor references
+ *  with an href. 
  */
 object LinkParser {
- 
-	def parse(sourceUrl: String, source: Source) : List[Link] = {
-		var lst: List[Link] = Nil
-		val data = XML.loadString(source.mkString)
-  
-		for(val entry <- data \\ "a") {
-			val href = (entry \\ "@href").text
-			println(href)
-		}
-		lst
-	}
+
+    val hrefRegex = """(\<a.*?href=\"(.*?)\".*?\>.*?\</a>)""".r
+
+    def parse(sourceUrl: String, source: Source) : List[Link] = {
+        var links : List[Link] = Nil
+
+        hrefRegex.findAllIn(source.mkString).foreach { link : String =>
+            link match {
+                case hrefRegex(x, url) => {
+                    links = new Link(sourceUrl, url, "") :: links
+                    println(x)
+                }
+                case _ => { println("blank") }
+            }
+        }
+
+        links
+    }
 }
